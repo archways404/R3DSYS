c607d28d77617613219552fc2c27b424d5847467
archways404 <archways@gmx.us>
2025-04-17 13:37:47 +0200

    init

--- new-web/src/app/login/page.js
+++ new-web/src/app/login/page.js
@@ -1,15 +1,22 @@
 'use client';
 
+import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { useDispatch } from 'react-redux';
 import { login } from '@/store/slices/authSlice';
 
+import EmailInput from '@/components/ui/email';
+import PasswordInput from '@/components/ui/password';
+
 export default function Login() {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 	const router = useRouter();
 	const dispatch = useDispatch();
 
 	const handleLogin = () => {
-		// Fake user
+		console.log('Entered email:', email);
+		console.log('Entered password:', password);
 		dispatch(
 			login({
 				name: 'Fake User',
@@ -21,10 +28,20 @@ export default function Login() {
 	};
 
 	return (
-		<button
-			onClick={handleLogin}
-			className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
-			Go to dashboard
-		</button>
+		<>
+			<EmailInput
+				value={password}
+				onChange={setEmail}
+			/>
+			<PasswordInput
+				value={password}
+				onChange={setPassword}
+			/>
+			<button
+				onClick={handleLogin}
+				className="mt-4 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
+				Go to dashboard
+			</button>
+		</>
 	);
 }
---
+++ new-web/src/components/ui/email.jsx
@@ -0,0 +1,29 @@
+import { useId } from 'react';
+import { MailIcon } from 'lucide-react';
+
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+
+export default function EmailInput({ value, onChange }) {
+	const id = useId();
+	return (
+		<div className="*:not-first:mt-2">
+			<Label htmlFor={id}>Input with end icon</Label>
+			<div className="relative">
+				<Input
+					id={id}
+					className="peer pe-9"
+					placeholder="Email"
+					type="email"
+					onChange={(e) => onChange(e.target.value)}
+				/>
+				<div className="text-muted-foreground/80 pointer-events-none absolute inset-y-0 end-0 flex items-center justify-center pe-3 peer-disabled:opacity-50">
+					<MailIcon
+						size={16}
+						aria-hidden="true"
+					/>
+				</div>
+			</div>
+		</div>
+	);
+}
